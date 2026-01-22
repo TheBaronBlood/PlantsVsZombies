f9cdcdd67226b3f061cb4ddf6530b4188c7bbea9
TheBaronBlood <derbaroncuki@gmail.com>
2026-01-22 03:55:13 +0100

    .gitignore modified & new tiles

---
+++ assets/tiles/Showen_map.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Showen_map" tilewidth="512" tileheight="256" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Showen_map.png" width="512" height="256"/>
+ </tile>
+</tileset>
